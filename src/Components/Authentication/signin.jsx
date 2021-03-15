60f7607f29a1a594fcc87f82fad17445fa4b5b2c
Nabeel Syed <nabeelsyed@Nabeels-MacBook-Pro.local>
2021-03-15 15:33:59 -0400

    good

--- src/Components/Authentication/signin.jsx
+++ src/Components/Authentication/signin.jsx
@@ -1,31 +1,161 @@
 import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import { Link, withRouter, NavLink } from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import GroupIcon from "@material-ui/icons/Group";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 
-function SignIn() {
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(7),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: "100%",
+  },
+}));
+
+export default function SignIn() {
+  const classes = useStyles();
+  const [firstLoad, setLoad] = React.useState(true);
+
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const handleEmailChange = (event) => setEmail(event.target.value);
+  const handlePasswordChange = (event) => setPassword(event.target.value);
+
+  const [message, setMessage] = React.useState("Nothing saved in the session");
+
+  async function sampleFunc(toInput) {
+    const response = await fetch("/api/validatelogin", {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, *cors, same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: "follow", // manual, *follow, error
+      referrerPolicy: "no-referrer", // no-referrer, *client
+      body: JSON.stringify(toInput), // body data type must match "Content-Type" header
+    });
+    let body = await response.json();
+    console.log(body.id);
+    setMessage(body.id ? "Data sucessfully updated" : "Data updation failed");
+  }
+
+  const handleSubmit = (variables) => {
+    const toInput = { email, password };
+    sampleFunc(toInput);
+    setEmail("");
+    setPassword("");
+  };
+
+  if (firstLoad) {
+    // sampleFunc();
+    setLoad(false);
+  }
+
+  function Copyright() {
+    return (
+      <Typography variant="body2" color="textSecondary" align="center">
+        {"Copyright © newByteX"} {new Date().getFullYear()}
+        {"."}
+      </Typography>
+    );
+  }
+
   return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <GroupIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign In
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                value={email}
+                label="Email"
+                name="email"
+                autoComplete="email"
+                onChange={handleEmailChange}
+              />
+            </Grid>
 
-<div className='a-container-signup'>
-   <div className='a-box-signin'>
-      <form>
-         <h3>Log In</h3>
-         <div className="form-group">
-            <label>Email</label>
-            <input type="email" className="contact-input" placeholder="Enter email" />
-         </div>
-         <div className="form-group">
-            <label>Password</label>
-            <input type="password" className="contact-input" placeholder="Enter password" />
-         </div>
-         <br></br><br></br>
-         <div className='s-b-text'>
-            <button type="submit" className="cv-btn">Register</button>
-         </div>
-         <p className="forgot-password text-right">
-            Not registered? <a href="sign-up">Sign up!</a>
-         </p>
-      </form>
-   </div>
-</div>
-  )
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="password"
+                value={password}
+                label="Password"
+                type="password"
+                name="password"
+                autoComplete="password"
+                onChange={handlePasswordChange}
+              />
+            </Grid>
+          </Grid>
+          <Button
+            // type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            preventefault
+            className={classes.submit}
+            onClick={handleSubmit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <NavLink to="/forgot">Forgot Password?</NavLink>
+            </Grid>
+            <Grid item>
+              <NavLink to="/sign-up">Don't have an account? Sign Up</NavLink>
+            </Grid>
+          </Grid>
+        </form>
+        <Typography style={{ margin: 7 }} variant="body1">
+          Status: {message}
+        </Typography>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
 }
-
-export default SignIn;
--- src/Components/Authentication/signup.jsx
+++ src/Components/Authentication/signup.jsx
@@ -3,36 +3,37 @@ import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-// import { Link } from "react-router-dom";
+import { Link, withRouter, NavLink } from "react-router-dom";
 import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
 import GroupIcon from "@material-ui/icons/Group";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(7),
     display: "flex",
     flexDirection: "column",
-    alignItems: "center"
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main
+    backgroundColor: theme.palette.secondary.main,
   },
   form: {
     width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(3)
+    marginTop: theme.spacing(3),
   },
   submit: {
-    margin: theme.spacing(3, 0, 2)
+    margin: theme.spacing(3, 0, 2),
   },
   textField: {
     marginLeft: theme.spacing(1),
     marginRight: theme.spacing(1),
-    width: "100%"
-  }
+    width: "100%",
+  },
 }));
 
 export default function AddEmployee() {
@@ -45,11 +46,11 @@ export default function AddEmployee() {
   const [phonenumber, setPhoneNumber] = React.useState("");
   const [password, setPassword] = React.useState("");
 
-  const handleNameChange = event => setName(event.target.value);
-  const handleAddressChange = event => setAddress(event.target.value);
-  const handleEmailChange = event => setEmail(event.target.value);
-  const handlePhoneNumberChange = event => setPhoneNumber(event.target.value);
-  const handlePasswordChange = event => setPassword(event.target.value);
+  const handleNameChange = (event) => setName(event.target.value);
+  const handleAddressChange = (event) => setAddress(event.target.value);
+  const handleEmailChange = (event) => setEmail(event.target.value);
+  const handlePhoneNumberChange = (event) => setPhoneNumber(event.target.value);
+  const handlePasswordChange = (event) => setPassword(event.target.value);
 
   const [message, setMessage] = React.useState("Nothing saved in the session");
 
@@ -60,19 +61,19 @@ export default function AddEmployee() {
       cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
       credentials: "same-origin", // include, *same-origin, omit
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
         // 'Content-Type': 'application/x-www-form-urlencoded',
       },
       redirect: "follow", // manual, *follow, error
       referrerPolicy: "no-referrer", // no-referrer, *client
-      body: JSON.stringify(toInput) // body data type must match "Content-Type" header
+      body: JSON.stringify(toInput), // body data type must match "Content-Type" header
     });
     let body = await response.json();
     console.log(body.id);
     setMessage(body.id ? "Data sucessfully updated" : "Data updation failed");
   }
 
-  const handleSubmit = variables => {
+  const handleSubmit = (variables) => {
     const toInput = { name, address, email, phonenumber, password };
     sampleFunc(toInput);
     setName("");
@@ -87,7 +88,14 @@ export default function AddEmployee() {
     setLoad(false);
   }
 
-//   Full Name, Address, Email, Phone, Pass
+  function Copyright() {
+    return (
+      <Typography variant="body2" color="textSecondary" align="center">
+        {"Copyright © newByteX"} {new Date().getFullYear()}
+        {"."}
+      </Typography>
+    );
+  }
 
   return (
     <Container component="main" maxWidth="xs">
@@ -97,7 +105,7 @@ export default function AddEmployee() {
           <GroupIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
-          Register
+          Sign Up
         </Typography>
         <form className={classes.form} noValidate>
           <Grid container spacing={2}>
@@ -181,55 +189,22 @@ export default function AddEmployee() {
             className={classes.submit}
             onClick={handleSubmit}
           >
-            Save
+            Sign Up
           </Button>
+          <Grid container>
+            <Grid item xs></Grid>
+            <Grid item>
+              <NavLink to="/sign-in">Already have an account? Sign in</NavLink>
+            </Grid>
+          </Grid>
         </form>
         <Typography style={{ margin: 7 }} variant="body1">
           Status: {message}
         </Typography>
       </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
     </Container>
   );
 }
-
-// function SignUp() {
-//   return (
-
-// <div className='a-container-signup'>
-//    <div className='a-box-signup'>
-//       <form>
-//          <h3>Register</h3>
-//          <div className="form-group">
-//             <label>Full Name</label>
-//             <input type="text" className="contact-input" placeholder="First name" />
-//          </div>
-//          <div className="form-group">
-//             <label>Email</label>
-//             <input type="email" className="contact-input" placeholder="Enter email" />
-//          </div>
-//          <div className="form-group">
-//             <label>Address</label>
-//             <input type="text" className="contact-input" placeholder="Enter address" />
-//          </div>
-//          <div className="form-group">
-//             <label>Phone Number</label>
-//             <input type="text" className="contact-input" placeholder="Enter phone number" />
-//          </div>
-//          <div className="form-group">
-//             <label>Password</label>
-//             <input type="password" className="contact-input" placeholder="Enter password" />
-//          </div>
-//          <br></br>
-//          <div className='s-b-text'>
-//             <button type="submit" className="cv-btn">Register</button>
-//          </div>
-//          <p className="forgot-password text-right">
-//             Already registered <a href="sign-in">log in?</a>
-//          </p>
-//       </form>
-//    </div>
-// </div>
-//   )
-// }
-
-// export default SignUp;
